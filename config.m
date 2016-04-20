03069d1f54fa0035ec7260c91baf7bd805c4f882
Oh233 <macshqi@gmail.com>
2016-04-20 23:43:30 +0800

    16/4/20 [Finish sampled image]

--- config.m
+++ config.m
@@ -1,12 +1,16 @@
 function opt = config(data_path)
 
-opt.data_path = data_path;
+addpath(genpath('./src/'));
+
+opt.root = './';
+opt.data_path = [opt.root 'data/data02/'];
+opt.cache_path = [opt.data_path 'cache/'];
+mkdir_if_missing(opt.cache_path);
 % get list names by dir command
 all_images = dir([data_path '/*.bmp']);
 opt.image_names = {all_images.name};
+opt.image_num = length(opt.image_names);
 % get light vector txt
 opt.light_vec = load([data_path '/lightvec.txt']);
-
-addpath(genpath('./src/'));
 
 end
--- src/process_data.m
+++ src/process_data.m
@@ -1,9 +1,36 @@
 function opt = process_data(opt)
 % This function perform uniformly sampling data in terms of light direction
-light_x = opt.light_vec(:, 1);
-light_y = opt.light_vec(:, 2);
-light_z = opt.light_vec(:, 3);
+
 % iteratively subdivde surface 4 (default) times
 vertices = icosahedron_sample(4);
+% get only points in half plane (light direction plane)
+vertices = vertices(find(vertices(:, 3) >= 0), :);
+
+% Algorithm:
+% for each one vertex, seek the nearest light direction
+% for that direction, interpolate images
+[IDX, ~] = knnsearch(opt.light_vec, vertices);
+IDX = unique(IDX);
+% Note: in theory, if light direction are the same, we don't need to
+% interpolate them again, however, due to practical error, even when
+% light direction are the same, the images are different
+direction_num = size(IDX, 1);
+% process one image to get image height and width
+I = imread(strcat(opt.data_path, opt.image_names{1}));
+warning('This loop way are too slow, try to figure out how to vectorize it');
+for i = 1:direction_num
+  tic_toc_print('Interpolate images and save to cache %d / %d\n', i, direction_num);
+  light_direction = opt.light_vec(IDX(i), :);
+  weight_sum = 0.0;
+  image_buffer = zeros(size(I));
+  for j = 1:opt.image_num
+    image_weight = opt.light_vec(j, :) * light_direction';
+    I = double(imread(strcat(opt.data_path, opt.image_names{j})));
+    image_buffer = image_buffer + I * image_weight;
+    weight_sum = weight_sum + image_weight;
+  end
+  image_buffer = uint8(image_buffer / weight_sum);
+  imwrite(image_buffer, [opt.cache_path 'image' num2str(i) '.bmp']);
+end
 
 end
